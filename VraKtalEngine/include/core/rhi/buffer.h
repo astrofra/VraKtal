6c5da6d09784e00cf408e42f2658ffa11ea41bcd
QUENTIN.Rigagneau <rga.quentin@gmail.com>
2025-09-26 15:06:10 +0200

    Add buffer and descriptor set interfaces to RHI

---
+++ VraKtalEngine/include/core/rhi/buffer.h
@@ -0,0 +1,41 @@
+#ifndef VRAKTAL_CORE_RHI_BUFFER_H
+#define VRAKTAL_CORE_RHI_BUFFER_H
+#pragma once
+
+namespace core::rhi
+{
+	enum class BufferUsage
+	{
+		Vertex, 
+		Index,
+		Uniform,
+		Storage,
+		TransferSrc,
+		TransferDst
+	};
+
+	enum class MemoryUsage
+	{
+		CPU,
+		GPU
+	};
+
+	struct BufferDesc
+	{
+		size_t size;
+		BufferUsage usage;
+		MemoryUsage memory;
+	};
+
+	class Buffer
+	{
+	public:
+		virtual ~Buffer() = default;
+
+		virtual void* Map() = 0;
+		virtual void* Unmap() = 0;
+		virtual void Update(const void* data, size_t size, size_t offset = 0) = 0;
+	};
+}
+
+#endif //VRAKTAL_CORE_RHI_BUFFER_H
---
+++ VraKtalEngine/include/core/rhi/descriptorSet.h
@@ -0,0 +1,49 @@
+#ifndef VRAKTAL_CORE_RHI_DESCRIPTOR_SET_H
+#define VRAKTAL_CORE_RHI_DESCRIPTOR_SET_H
+#pragma once
+
+#include <cstdint>
+#include <vector>
+
+#include <core/rhi/buffer.h>
+#include <core/rhi/image.h>
+
+namespace core::rhi
+{
+	enum class DescriptorType
+	{
+		UniformBuffer, 
+		StorageBuffer,
+		ImageSampler
+	};
+
+	enum class ShaderStage
+	{
+		Vertex,
+		Fragment,
+		Compute
+	};
+
+	struct DescriptorBinding
+	{
+		uint32_t binding;
+		DescriptorType type;
+		ShaderStage stage;
+	};
+
+	struct DescriptorSetLayoutDesc
+	{
+		std::vector<DescriptorBinding> bindings;
+	};
+
+	class DescriptorSet
+	{
+	public:
+		virtual ~DescriptorSet() = default;
+
+		virtual void BindBuffer(uint32_t binding, Buffer* buffer, size_t offset, size_t range) = 0;
+		virtual void BindImage(uint32_t binding, Image* image, ImageUsage* usage) = 0;
+	};
+}
+
+#endif //VRAKTAL_CORE_RHI_DESCRIPTOR_SET_H
